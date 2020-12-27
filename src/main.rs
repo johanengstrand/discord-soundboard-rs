48f50bb3447343ee46829198cea4a318fc8c773c
Johan Engstrand <johan@engstrand.nu>
2020-12-27 21:31:41 +0100

    feat: read a directory and list all files in it

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,33 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 
+use std::io;
 use std::env;
+use std::fs::{self, DirEntry};
+use std::path::PathBuf;
 use rocket_contrib::serve::StaticFiles;
 
 mod api;
 mod discord;
+
+#[derive(Debug)]
+struct Track {
+    name: PathBuf,
+}
+
+fn get_tracks_in_dir(dir: &str) -> Result<Vec<Track>, String> {
+    let path = PathBuf::from(dir);
+    let mut tracks = Vec::new();
+
+    match fs::read_dir(path) {
+        Err(why) => println!("error"),
+        Ok(paths) => for path in paths {
+            tracks.push(Track { name: path.unwrap().path() });
+        },
+    }
+
+    Ok(tracks)
+}
 
 fn start_webserver() {
     api::routes::mount()
@@ -17,6 +39,8 @@ fn start_webserver() {
 fn main() {
     // TODO: Error handling
     // Configure the client with your Discord bot token in the environment.
+    let track_list = get_tracks_in_dir("/home/johan/nextcloud/VERY_IMPORTANT_FILES");
+    println!("{:?}", track_list.unwrap());
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
 
