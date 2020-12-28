73aa7e76e136a9757b8c7cf4c9f50e0b94bf238d
Johan Engstrand <johan@engstrand.nu>
2020-12-28 17:23:52 +0100

    feat: category field in API

--- src/bot/tracks.rs
+++ src/bot/tracks.rs
@@ -2,15 +2,27 @@ use std::fs;
 use std::path::PathBuf;
 use std::io::{self, Error, ErrorKind};
 
-fn visit_dirs(dir: &PathBuf, tracks: &mut Vec<PathBuf>) -> io::Result<()> {
+#[derive(Debug, Serialize)]
+pub struct Track {
+    name: String,
+    category: PathBuf,
+    path: PathBuf,
+}
+
+fn visit_dirs(dir: &PathBuf, category: &PathBuf, tracks: &mut Vec<Track>) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
+            let file_name = entry.file_name().into_string().unwrap();
             if path.is_dir() {
-                visit_dirs(&path, tracks)?;
+                visit_dirs(&path, &category.join(file_name), tracks)?;
             } else {
-                tracks.push(path);
+                tracks.push(Track {
+                    name: file_name,
+                    category: category.to_path_buf(),
+                    path: path,
+                });
             }
         }
         Ok(())
@@ -19,9 +31,9 @@ fn visit_dirs(dir: &PathBuf, tracks: &mut Vec<PathBuf>) -> io::Result<()> {
     }
 }
 
-pub fn get_tracks_in_dir(dir: &str) -> Result<Vec<PathBuf>, String> {
+pub fn get_tracks_in_dir(dir: &str) -> Result<Vec<Track>, String> {
     let mut tracks = Vec::new();
-    match visit_dirs(&PathBuf::from(dir), &mut tracks) {
+    match visit_dirs(&PathBuf::from(dir), &PathBuf::new(), &mut tracks) {
         Ok(_) => Ok(tracks),
         Err(why) => Err(format!("Failed to read tracks in {}: {}", dir, why)),
     }
